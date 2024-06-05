a393d2e3442db26385fb3c7ba155094d9ed8b789
Andriy Massimilla <andriy@a2-ai.com>
2024-06-05 16:09:36 -0400

    Remove server

--- frontend/pages/project/@id/+title.ts
+++
@@ -1,7 +0,0 @@
-import type { PageContext } from "vike/types";
-// import type { Data } from "./+data";
-
-export function title(pageContext: PageContext) {
-	const { project } = pageContext.data || {};
-	return `${project?.name || "???"} - Spackle`;
-}
