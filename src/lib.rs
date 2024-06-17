aa223707e693a2361ae023eca1f81869a4ecf26b
Andriy Massimilla <andriy@a2-ai.com>
2024-06-17 13:33:18 -0400

    Combine out_dir and name

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,15 @@ pub mod util;
 
 /// Generates a filled directory from the specified spackle project.
 ///
-/// out_dir is the directory to put the filled directory, and out_name is the name of the filled directory.
+/// out_dir is the path to what will become the filled directory
 pub fn generate(
     project_dir: &PathBuf,
     data: &HashMap<String, String>,
     out_dir: &PathBuf,
-    out_name: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let gen_dir = out_dir.join(out_name);
-
     let config = config::load(project_dir).map_err(|e| e.to_string())?;
 
-    copy::copy(project_dir, &gen_dir, &config.ignore)?;
+    copy::copy(project_dir, &out_dir, &config.ignore)?;
 
     // Fill the template, returning if any files failed to render
     let results = template::fill(project_dir, data, out_dir)?;
