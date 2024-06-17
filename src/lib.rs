dbc741c83c5066e9633a0ebb06a55ff91b2c6b82
Andriy Massimilla <andriy@a2-ai.com>
2024-06-17 12:35:10 -0400

    Throw error if any file fails in template fill step

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ use util::copy;
 pub mod core;
 pub mod util;
 
+/// Generates a filled directory from the specified spackle project.
+///
+/// out_dir is the directory to put the filled directory, and out_name is the name of the filled directory.
 pub fn generate(
     project_dir: &PathBuf,
     data: &HashMap<String, String>,
@@ -14,12 +17,17 @@ pub fn generate(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let gen_dir = out_dir.join(out_name);
 
-    // Load the project
     let config = config::load(project_dir).map_err(|e| e.to_string())?;
 
     copy::copy(project_dir, &gen_dir, &config.ignore)?;
 
-    template::fill(project_dir, data, out_dir)?;
+    // Fill the template, returning if any files failed to render
+    let results = template::fill(project_dir, data, out_dir)?;
+    for result in results {
+        if let Err(e) = result {
+            return Err(e.into());
+        }
+    }
 
     Ok(())
 }
